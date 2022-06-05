82795f33a5c0b565a34269b5956db44e4fbbedb7
Franco <piconito.f@gmail.com>
2022-06-05 15:28:02 -0300

    Mi primera version

---
+++ ProyectoCoder/urls.py
@@ -0,0 +1,8 @@
+from AppCoder.views import curso
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('curso/',curso),
+]
